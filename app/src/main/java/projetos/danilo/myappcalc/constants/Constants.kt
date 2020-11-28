c678c836c3469777bbb60e569f97a22e51497d64
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-11-27 21:48:49 -0300

    pacotes e constantes

---
+++ app/src/main/java/projetos/danilo/myappcalc/constants/Constants.kt
@@ -0,0 +1,24 @@
+package projetos.danilo.myappcalc.constants
+
+//constantes que serão utilizadas
+
+const val NUM0 = "0"
+const val NUM1 = "1"
+const val NUM2 = "2"
+const val NUM3 = "3"
+const val NUM4 = "4"
+const val NUM5 = "5"
+const val NUM6 = "6"
+const val NUM7 = "7"
+const val NUM8 = "8"
+const val NUM9 = "9"
+const val POINT = "."
+const val SIGNAL = "+/-"
+const val MINUS = "-"
+const val PLUS ="+"
+const val DIVIDE = "÷"
+const val MULTIPLY = "×"
+const val AC = "AC"
+const val BACK = "BACK"
+const val EQUAL = "="
+const val ERROR = "Error"
